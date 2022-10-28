e37c79c9f26be871c5627074656caf379efa1f6d
XXXPEOPLEXXX <116590712+XXXPEOPLEXXX@users.noreply.github.com>
2022-10-28 15:06:33 +0300

    Delete 2.kt

--- 2.kt
+++ 2.kt
@@ -3,8 +3,8 @@ fun main() {
 
     println("Введите год")
     val god = readln().toInt()
-    val result = if(god % 4 == 0 ) "Високосный год 366 дней" else "Не высокосный год 365 дней"
+    val kraten = if(god % 4 == 0 && god % 100 != 0 || god % 400 == 0 ) "Високосный год 366 дней" else "Не высокосный год 365 дней"
 
-    println(result)
+    println(kraten)
 //    println("Ваше число ${even_number(x, y)}")
 }
